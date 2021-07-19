40614f8f5c1986fe6b00cb48b80cdbe1e9b88a7a
placideh <h.uwizeyeplacide@gmail.com>
2021-07-19 12:00:01 +0200

    fixed submit error

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Form from './components/Form'
 import Photos from './components/Photos'
 function App() {
   const[photos,setPhotos]=useState([])
-  const [id,setId]=useState(0)
+  const[id,setId]=useState(0)
+
   //getAlbumId from the Form
   const getAlbumId=(id)=>{
     setId(id)
   }
-  useEffect(()=>{
-    const getAlbum=async ()=>{
-      const albumPhoto=await fetchAlbum()
-      setPhotos(albumPhoto)
+
+//fetchTheAlbumPhotos
+useEffect(()=>{
+    const fetchAlbum=async ()=>{
+      const res=await fetch(`https://jsonplaceholder.typicode.com/albums/${id}/photos`)
+      const data=await res.json()
+      return data
     }
-    getAlbum()
-  },[id])
-  //fetchTheAlbumPhotos
-  const fetchAlbum=async ()=>{
-    const res=await fetch(`https://jsonplaceholder.typicode.com/albums/${id}/photos`)
-    const data=await res.json()
-    return data
+  const getAlbum=async ()=>{
+    const albumPhoto=await fetchAlbum()
+    setPhotos(albumPhoto)
   }
+  getAlbum()
+},[id])
   return (
     <div>
       <Header/>
       <Form onSearch={getAlbumId}/>
       <Photos photos={photos}/>
-
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,21 @@
  import {useState} from 'react'
  const Form = ({onSearch}) => {
-   const [albumId,setAlbumId]=useState('');
+   const [albumId,setAlbumId]=useState('')
    const onSubmit=(e)=>{
-     e.preventDefault();
+     e.preventDefault()
      if(!albumId){
        alert("Please Enter the Id!")
        return
      }
-     onSearch(albumId);
-     setAlbumId('');
+     onSearch(albumId)
+     setAlbumId('')
    }
+
+
     return (
         <form className="my-form" onSubmit={onSubmit}>
             <input type="search" value={albumId} onChange={(e)=>setAlbumId(e.target.value)} className="inputText"placeholder="Enter Album Id"/>
-            <button className="btn">Get Album Photos By Id</button>
+            <button  className="btn">Get Album Photos By Id</button>
         </form>
     )
 }
